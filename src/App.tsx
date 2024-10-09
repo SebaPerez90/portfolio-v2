dfdf5437c778afe3e81c21d9ab0bdd5492a98433
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-10-09 19:28:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+		<div>
+			<h1 className="text-9xl text-red-700 pepe">hello worldddd</h1>
+			<p className="text-blue-700">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti minus error impedit distinctio commodi maiores, autem
+				officia ex vitae quod eius eaque obcaecati possimus odit, nesciunt voluptatibus. Quisquam, provident enim.
+			</p>
+			<Button>Button</Button>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import "@/styles/globals.css";
+import '@/styles/custom.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
