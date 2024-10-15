8ddb4f96ce9fc7fb429261e2d6aa82d7c4b357d3
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-10-15 09:12:09 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import Header from '@/components/header';
-import Hero from '@/components/hero/Hero';
+import { Suspense, lazy } from 'react';
 import { Toaster } from 'react-hot-toast';
-import Footer from '@/components/core/Footer';
-import FormContainer from '@/components/FormContainer';
-import SkillsContainer from './components/SkillsContainer';
-import { Experience } from './components/Experience';
+
+const Header = lazy(() => import('@/components/header'));
+const Hero = lazy(() => import('@/components/hero/Hero'));
+const Footer = lazy(() => import('@/components/core/Footer'));
+const FormContainer = lazy(() => import('@/components/FormContainer'));
+const SkillsContainer = lazy(() => import('@/components/SkillsContainer'));
+const Experience = lazy(() => import('@/components/Experience'));
 
 function App() {
   return (
-    <>
+    <Suspense fallback={<div className="h-screen col-center bg-red-500">cargando</div>}>
       <Header />
       <main className="col-center gap-0 bg-white dark:bg-dark-neutral">
         <Hero />
@@ -18,7 +20,7 @@ function App() {
       </main>
       <Footer />
       <Toaster />
-    </>
+    </Suspense>
   );
 }
 
